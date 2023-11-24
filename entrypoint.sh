09a00fb0e2dc3c994e01c251b357e22d5cb01aa4
eru123 <yeoligoakino@gmail.com>
2023-11-25 01:40:10 +0800

    Add initialization scripts and conditionals for environment variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 
-composer install --no-interaction --no-progress
+if [ ! -f "/app/.env" ]; then
+    printenv > /app/.env
+fi 
+
+php scripts/init.php
+chmod +x /app/scripts_sh/*.sh
+
+if [ -n "$NGINX_PROXY_MANAGER_DATA_PATH" ]; then
+    /app/scripts_sh/nginx_proxy_manager.sh &
+fi
+
+if [ -z "$ROOTFS_PATH" ]; then
+    ROOTFS_PATH=/app/rootfs
+fi
+
+if [ -f "$ROOTFS_PATH/proc/meminfo" ]; then
+    /app/scripts_sh/sys_mem.sh &
+fi
 
 php index.php
