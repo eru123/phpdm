15840be1e72b81417b58d9ac62a24c56dc931703
eru123 <yeoligoakino@gmail.com>
2023-11-25 01:42:11 +0800

    Add timezone setting and ORM configuration

--- app/autoload.php
+++ app/autoload.php
@@ -1,4 +1,19 @@
 <?php
 
+date_default_timezone_set('Asia/Manila');
+
 require_once __DIR__ . '/vendor/autoload.php';
 
+use eru123\orm\ORM;
+
+venv_protect();
+venv_load(__DIR__ . '/.env', false);
+
+ORM::set_pdo([
+    'driver' => 'mysql',
+    'host' => venv('DB_HOST'),
+    'port' => venv('DB_PORT'),
+    'user' => venv('DB_USER'),
+    'pass' => venv('DB_PASS'),
+    'name' => venv('DB_NAME')
+]);
