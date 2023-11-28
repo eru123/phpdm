fb2d823940198158ac2df308f268cba2e3b006b5
eru123 <yeoligoakino@gmail.com>
2023-11-28 22:28:28 +0800

    Add ROOTFS_PATH variable to entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,15 +4,15 @@ if [ ! -f "/app/.env" ]; then
     printenv > /app/.env
 fi 
 
+if [ -z "$ROOTFS_PATH" ]; then
+    ROOTFS_PATH=/app/rootfs
+fi
+
 php scripts/init.php
 chmod +x /app/scripts_sh/*.sh
 
 if [ -n "$NGINX_PROXY_MANAGER_DATA_PATH" ]; then
     /app/scripts_sh/nginx_proxy_manager.sh &
-fi
-
-if [ -z "$ROOTFS_PATH" ]; then
-    ROOTFS_PATH=/app/rootfs
 fi
 
 if [ -f "$ROOTFS_PATH/proc/meminfo" ]; then
