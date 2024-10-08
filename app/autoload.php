37b2e3aaf81df6dfafd5dbce4dfccd323b1707d4
eru123 <39824083+eru123@users.noreply.github.com>
2024-10-08 14:51:41 +0200

    implement wyue framework

--- app/autoload.php
+++ app/autoload.php
@@ -1,19 +1,23 @@
 <?php
 
-date_default_timezone_set('Asia/Manila');
-
 require_once __DIR__ . '/vendor/autoload.php';
 
-use eru123\orm\ORM;
+use Wyue\MySql;
+use Wyue\Venv;
 
-venv_protect();
-venv_load(__DIR__ . '/.env', false);
+Venv::protect(true);
+Venv::load_env(__DIR__ . '/.env', false);
 
-ORM::set_pdo([
-    'driver' => 'mysql',
-    'host' => venv('DB_HOST'),
-    'port' => venv('DB_PORT'),
-    'user' => venv('DB_USER'),
-    'pass' => venv('DB_PASS'),
-    'name' => venv('DB_NAME')
+date_default_timezone_set(Venv::get('TZ', 'Asia/Manila'));
+
+MySql::set_config([
+    'host' => Venv::get('DB_HOST', 'localhost'),
+    'port' => Venv::get('DB_PORT', 3306),
+    'user' => Venv::get('DB_USER', 'root'),
+    'pass' => Venv::get('DB_PASS', null),
+    'name' => Venv::get('DB_NAME', 'phpdm'),
+    'migrations_table' => 'migrations',
+    'migrations_path' => __DIR__ . '/db/migrations',
+    'models_path' => __DIR__ . '/src/Models',
+    'models_namespace' => 'App\Models',
 ]);
